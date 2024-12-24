0e9f1a44fde1c60a6368b36308f02935fbc50e50
Sadriansyah <sadriansyah@samudera.id>
2024-12-24 22:58:55 +0800

    feat: update origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://fe-hyperhire.kabpaser.id',
   });
 
   await app.listen(process.env.PORT || 3000);
